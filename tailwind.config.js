8ea6f83f92c64e7d213d93ed536a48129154b71b
Jatin Chhanwal <connectwithjatin365@gmail.com>
2025-04-19 19:20:57 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
+          50: '#f0f7ff',
+          100: '#e0effe',
           200: '#bae6fd',
           300: '#7dd3fc',
           400: '#38bdf8',
@@ -30,6 +30,18 @@ export default {
           700: '#334155',
           800: '#1e293b',
           900: '#0f172a',
+        },
+        accent: {
+          50: '#fdf4ff',
+          100: '#fae8ff',
+          200: '#f5d0fe',
+          300: '#f0abfc',
+          400: '#e879f9',
+          500: '#d946ef',
+          600: '#c026d3',
+          700: '#a21caf',
+          800: '#86198f',
+          900: '#701a75',
         },
       },
       fontFamily: {
